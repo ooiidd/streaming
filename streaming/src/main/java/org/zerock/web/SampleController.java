7ea9571321baa1433d7ce287f807ed868863a56f
SongHun <ooiidds@gmail.com>
2018-06-22 19:45:33 +0900

    06-22-echo

--- streaming/src/main/java/org/zerock/web/SampleController.java
+++ streaming/src/main/java/org/zerock/web/SampleController.java
@@ -2,18 +2,28 @@ package org.zerock.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SampleController {
 	private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
-	private int test=0;
-	@RequestMapping("doa")
-	public String doa() {
-		logger.info("doa called.... test : "+test);
-		test++;
-		return "result";
+	@RequestMapping(value="data", method = RequestMethod.POST)
+	public ResponseEntity<String> getData(@RequestBody String data,Model model) {
+		
+		ResponseEntity<String> entity = null;
+		try {
+			entity = new ResponseEntity<String>(data,HttpStatus.OK);
+		}catch(Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);
+		}
+		return entity;
 	}
 	
 	@RequestMapping("dob")
--- streaming/src/main/webapp/WEB-INF/views/home.jsp
+++ streaming/src/main/webapp/WEB-INF/views/home.jsp
@@ -6,9 +6,9 @@
 </head>
 <body>
 <h1>
-	Hello world!  
+	Hello yujung!  
 </h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<P> ${serverTime} </P>
 </body>
 </html>
